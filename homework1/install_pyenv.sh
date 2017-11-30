8242fc6de95b7a51e76a87f287e0c003a9011f7e
Yury Karatseyeu <pifagor13@mail.ru>
2017-11-30 12:36:36 +0300

    final script

--- homework1/install_pyenv.sh
+++ homework1/install_pyenv.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+sudo yum install -y zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel xz xz-devel
+curl -L https://raw.github.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+sudo echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> $HOME/.bash_profile
+sudo echo 'eval "$(pyenv init -)"' >> $HOME/.bash_profile
+sudo echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.bash_profile
+source $HOME/.bash_profile
+pyenv install 2.7.14
+pyenv global 3.7.14
+pyenv virtualenv 1st
+pyenv deactivate
+pyenv install 3.5.4
+pyenv global 3.5.4
+pyenv virtualenv 2st
+
